57b0348d7c909d1b86611ed68519d91ad25ac563
ommyzhang <tdxdxoz@gmail.com>
2017-09-06 01:14:42 +0800

    shorten flash time

--- app/src/main/java/org/attentiveness/news/splash/SplashActivity.java
+++ app/src/main/java/org/attentiveness/news/splash/SplashActivity.java
@@ -34,7 +34,7 @@ public class SplashActivity extends BaseActivity {
     }
 
     private void init() {
-        mTimer = new CountDownTimer(3000, 1000) {
+        mTimer = new CountDownTimer(2000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
 
