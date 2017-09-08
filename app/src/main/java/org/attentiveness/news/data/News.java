9efcea3ffd9647d55a4588386e038724f683cc61
Shols <790479613@qq.com>
2017-09-08 23:18:06 +0800

    yjy debug1

--- app/src/main/java/org/attentiveness/news/data/News.java
+++ app/src/main/java/org/attentiveness/news/data/News.java
@@ -2,7 +2,11 @@ package org.attentiveness.news.data;
 
 import com.google.gson.annotations.SerializedName;
 
+import org.attentiveness.news.net.GetNews;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class News {
@@ -28,12 +32,25 @@ public class News {
         this.date = date;
     }
 
-    public List<Story> getStoryList() {
-        ArrayList<Story> sL = new ArrayList<Story>(storyList);
-        sL.add(0,new Story(0,"zyn_test1"));
-        sL.add(0,new Story(1,"zyn_test2"));
-        return sL;
-        //return storyList;
+    public List<Story> getStoryList() throws Exception {
+        ArrayList<Story> storylist = new ArrayList<Story>(storyList);
+        HashSet<String> tags = new HashSet<>();
+        tags.add("科技");
+        tags.add("教育");
+        tags.add("文化");
+        HashSet<String> notshow = new HashSet<>();
+        int pagesize = 10;
+        GetNews getnews = new GetNews(tags, notshow, pagesize);
+        ArrayList<HashMap> detaillist = getnews.getMore();
+        int i = 0;
+        for (HashMap map : detaillist) {
+            storylist.add(0, new Story(i, (String) map.get("news_title")));
+            System.out.println(map.get("news_title"));
+            i++;
+        }
+        storylist.add(0,new Story(0,"zyn_test1"));
+        storylist.add(0,new Story(1,"zyn_test2"));
+        return storylist;
     }
 
     public void setStoryList(List<Story> storyList) {
